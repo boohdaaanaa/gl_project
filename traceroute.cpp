70b315655635c243d42f5677c16633449625c703
boohdaaanaa <bohdanaf06@gmail.com>
2025-03-11 13:15:08 +0200

    Update traceroute.cpp

--- traceroute.cpp
+++ traceroute.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+using namespace std;
+
+int main() {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (sockfd < 0) {
+        perror("Socket error");
+        exit(1);
+    }
+    cout << "Raw socket created successfully" << endl;
+    close(sockfd);
+    return 0;
+}
 
