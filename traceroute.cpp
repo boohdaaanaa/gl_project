4b52b704dc4c7f9387efc2f8c316224b3be4fcd3
boohdaaanaa <bohdanaf06@gmail.com>
2025-03-12 16:37:30 +0200

    Update traceroute.cpp

--- traceroute.cpp
+++ traceroute.cpp
@@ -4,17 +4,53 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <netinet/ip_icmp.h>
 
 using namespace std;
 
-int main() {
+unsigned short checksum(void *b, int len) {
+    unsigned short *buf = (unsigned short *)b;
+    unsigned int sum = 0;
+    unsigned short result;
+
+    for (sum = 0; len > 1; len -= 2)
+        sum += *buf++;
+    if (len == 1)
+        sum += *(unsigned char *)buf;
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    result = ~sum;
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <IP>" << endl;
+        return 1;
+    }
+
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if (sockfd < 0) {
         perror("Socket error");
         exit(1);
     }
-    cout << "Raw socket created successfully" << endl;
+
+    struct sockaddr_in dest_addr;
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.sin_family = AF_INET;
+    inet_pton(AF_INET, argv[1], &dest_addr.sin_addr);
+
+    struct icmphdr icmp_hdr;
+    memset(&icmp_hdr, 0, sizeof(icmp_hdr));
+    icmp_hdr.type = ICMP_ECHO;
+    icmp_hdr.un.echo.id = getpid();
+    icmp_hdr.checksum = checksum(&icmp_hdr, sizeof(icmp_hdr));
+
+    int ttl = 1;
+    setsockopt(sockfd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl));
+    sendto(sockfd, &icmp_hdr, sizeof(icmp_hdr), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    
+    cout << "ICMP request sent to " << argv[1] << " with TTL = " << ttl << endl;
     close(sockfd);
     return 0;
-}
-
+}   
